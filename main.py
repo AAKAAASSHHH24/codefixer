fbbc39aed579515eee0b8dae90b28dbe4d3f0e6b
AAKAAASSHHH24 <akashrksht@gmail.com>
2023-05-02 20:46:59 +0530

    basic backend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, render_template
 from dotenv import load_dotenv
 import openai
-import hashlib
-import sqlite3
-import stripe
 import os
 
 app = Flask(__name__)
@@ -11,65 +8,11 @@ load_dotenv()
 
 # API Token
 openai.api_key = os.getenv('openai_api_key')
-stripe.api_key = os.getenv('stripe_api_key')
-
-
-def initialize_database():
-    conn = sqlite3.connect('app.db')
-    c = conn.cursor()
-    c.execute(
-        '''CREATE TABLE IF NOT EXISTS users (fingerprint text primary key, usage_counter int)''')
-    conn.commit()
-    conn.close()
-
-
-def get_fingerprint():
-    browser = request.user_agent.browser
-    version = request.user_agent.version and float(
-        request.user_agent.version.split(".")[0])
-    platform = request.user_agent.platform
-    string = f"{browser}:{version}:{platform}"
-    fingerprint = hashlib.sha256(string.encode("utf-8")).hexdigest()
-    print(fingerprint)
-    return fingerprint
-
-
-def get_usage_counter(fingerprint):
-    conn = sqlite3.connect('app.db')
-    c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS users '
-              '(fingerprint text, usage_counter int)')
-    result = c.execute('SELECT usage_counter FROM users WHERE fingerprint=?',
-                       [fingerprint]).fetchone()
-    conn.close()
-    if result is None:
-        conn = sqlite3.connect('app.db')
-        c = conn.cursor()
-        c.execute('INSERT INTO users (fingerprint, usage_counter) VALUES '
-                  '(?, 0)', [fingerprint])
-        conn.commit()
-        conn.close()
-        return 0
-    else:
-        return result[0]
-
-
-def update_usage_counter(fingerprint, usage_counter):
-    conn = sqlite3.connect('app.db')
-    c = conn.cursor()
-    c.execute('UPDATE users SET usage_counter=? WHERE fingerprint=?',
-              [usage_counter, fingerprint])
-    conn.commit()
-    conn.close()
+#stripe.api_key = os.getenv('stripe_api_key')
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    initialize_database()
-    fingerprint = get_fingerprint()
-    usage_counter = get_usage_counter(fingerprint)
-
-    print(usage_counter)
     if request.method == "POST":
         if usage_counter > 3:
             return render_template("payment.html")
@@ -110,28 +53,6 @@ def index():
                                fixed_code=fixed_code)
 
     return render_template("index.html")
-
-
-@app.route("/charge", methods=["POST"])
-def charge():
-    amount = int(request.form["amount"])
-    plan = str(request.form["plan"])
-
-    customer = stripe.Customer.create(
-        email=request.form["stripeEmail"],
-        source=request.form["stripeToken"]
-    )
-
-    charge = stripe.Charge.create(
-        customer=customer.id,
-        amount=amount,
-        currency="usd",
-        description="App Charge"
-    )
-
-    return render_template("charge.html", amount=amount, plan=plan)
-
-
 
 
 if __name__ == "__main__":
